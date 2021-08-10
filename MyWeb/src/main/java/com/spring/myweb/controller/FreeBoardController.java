1dd047ee84150dcc7894076aeaa2297470b542ce
suyeon0610 <dbtndus0610@gmail.com>
2021-08-10 16:56:30 +0900

    myweb upload

---
+++ MyWeb/src/main/java/com/spring/myweb/controller/FreeBoardController.java
@@ -0,0 +1,26 @@
+package com.spring.myweb.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.myweb.freeboard.service.IFreeBoardService;
+
+@Controller
+@RequestMapping("/freeBoard")
+public class FreeBoardController {
+
+	@Autowired
+	private IFreeBoardService service;
+	
+	//목록 화면
+	@GetMapping("/freeList")
+	public void freeList(Model model) {
+		model.addAttribute("boardList", service.getList());
+	}
+	
+}
